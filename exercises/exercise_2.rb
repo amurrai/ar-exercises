ee10b9b2cb6c769c4db761eb5f153b648afdcb56
amurrai <andre.murrai@gmail.com>
2024-04-07 13:12:31 -0400

    Ex2 Update first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.update(annual_revenue: 290000)
+pp @store1
